503692862f47111bf00113b9db7958983e7f0dcd
zahmed3 <99377958+zahmed3@users.noreply.github.com>
2025-04-18 22:28:40 -0400

    Updated to import new Python files

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from gui import *
 from interface_functions import *
 from saf import *
+from options import *
+from google_drive_upload import *
 
+#Creates the GUI for the user to interact with.
 if __name__ == "__main__":
     create_gui()
