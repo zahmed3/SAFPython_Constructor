1358a6dcb7ee83339137c36d8d4cd781c4f54814
zahmed3 <99377958+zahmed3@users.noreply.github.com>
2025-04-20 13:54:30 -0400

    Added error messages if input file and folder output selection windows are closed before a selection is made and moved close_app to options for better organization

--- interface_functions.py
+++ interface_functions.py
@@ -2,33 +2,13 @@ from tkinter import *
 from tkinter import filedialog
 import gui as gui
 import saf as saf
-
-#This function will create a pop-up when the Google Sign-In button is pressed, letting the user know it is still a WIP.
-def google_signin_error_popup():
-    error_popup = Toplevel(gui.window)
-    popup_width = 400
-    popup_height = 150
-    screen_width = gui.window.winfo_screenwidth()
-    screen_height = gui.window.winfo_screenheight()
-    x = (screen_width / 2) - (popup_width / 2)
-    y = (screen_height / 2) - (popup_height / 2)
-    error_popup.geometry(f'{popup_width}x{popup_height}+{int(x)}+{int(y)}')
-    error_popup.title("Google Sign-In Error")
-    label = Label(error_popup, text="Google Sign-Ins are currently unavailable in this version.", font=("Inter", 10))
-    label.pack(padx=20, pady=20)
-    close_button = Button(error_popup, text="Close Pop-up", font=("Inter", 10), command=error_popup.destroy)
-    close_button.config(width="30", height="15")
-    close_button.pack(pady=20)
-    error_popup.grab_set()
-    gui.window.wait_window(error_popup)
-
-#This function will close the application when the button is pressed.
-def close_application():
-    gui.window.destroy()
+import options as opt
 
 #This function allows the user to select the CSV file they wish to convert.
 def select_csv_file():
-    gui.window.csvfilename = filedialog.askopenfilename(initialdir="/", title="Select CSV File", filetypes=(("csv files", "*.csv"),("all files", "*.*")))
+    gui.window.csvfilename = filedialog.askopenfilename(initialdir="/",
+                                                        title="Select CSV File",
+                                                        filetypes=(("csv files", "*.csv"),("all files", "*.*")))
     gui.selected_file = gui.window.csvfilename
     selected_file_popup = Toplevel(gui.window)
     popup_width = 1000
@@ -38,10 +18,17 @@ def select_csv_file():
     x = (screen_width / 2) - (popup_width / 2)
     y = (screen_height / 2) - (popup_height / 2)
     selected_file_popup.geometry(f'{popup_width}x{popup_height}+{int(x)}+{int(y)}')
-    selected_file_popup.title("Selected CSV File Chosen")
-    label = Label(selected_file_popup, text="Selected '" + gui.selected_file + "' as chosen file.", font=("Inter", 10))
+    if gui.selected_file == "":
+        selected_file_popup.title("Error: CSV File Not Selected")
+        label = Label(selected_file_popup, text="Error when selecting CSV file, please try again.",
+                      font=("Inter", 10))
+    else:
+        selected_file_popup.title("Selected CSV File Chosen")
+        label = Label(selected_file_popup, text="Selected '" + gui.selected_file + "' as chosen file.",
+                      font=("Inter", 10))
     label.pack(padx=20, pady=20)
-    close_button = Button(selected_file_popup, text="Close Pop-up", font=("Inter", 10), command=selected_file_popup.destroy)
+    close_button = Button(selected_file_popup, text="Close Pop-up", font=("Inter", 10),
+                          command=selected_file_popup.destroy)
     close_button.config(width="30", height="15")
     close_button.pack(pady=20)
     selected_file_popup.grab_set()
@@ -59,10 +46,19 @@ def select_folder_destination():
     x = (screen_width / 2) - (popup_width / 2)
     y = (screen_height / 2) - (popup_height / 2)
     selected_folder_popup.geometry(f'{popup_width}x{popup_height}+{int(x)}+{int(y)}')
-    selected_folder_popup.title("Selected Folder Destination Chosen")
-    label = Label(selected_folder_popup, text="Selected '" + gui.selected_folder + "' as chosen folder destination.", font=("Inter", 10))
+    if gui.selected_folder == "":
+        selected_folder_popup.title("Error: Folder Destination Not Selected")
+        label = Label(selected_folder_popup,
+                      text="Error when selecting folder destination, please try again.",
+                      font=("Inter", 10))
+    else:
+        selected_folder_popup.title("Selected Folder Destination Chosen")
+        label = Label(selected_folder_popup,
+                      text="Selected '" + gui.selected_folder + "' as chosen folder destination.",
+                      font=("Inter", 10))
     label.pack(padx=20, pady=20)
-    close_button = Button(selected_folder_popup, text="Close Pop-up", font=("Inter", 10), command=selected_folder_popup.destroy)
+    close_button = Button(selected_folder_popup, text="Close Pop-up", font=("Inter", 10),
+                          command=selected_folder_popup.destroy)
     close_button.config(width="30", height="15")
     close_button.pack(pady=20)
     selected_folder_popup.grab_set()
@@ -84,7 +80,8 @@ def start_operation():
         file_error_popup.title("File Input Error")
         label = Label(file_error_popup, text="Please select the file you wish to input.", font=("Inter", 10))
         label.pack(padx=20, pady=20)
-        close_button = Button(file_error_popup, text="Close Pop-up", font=("Inter", 10), command=file_error_popup.destroy)
+        close_button = Button(file_error_popup, text="Close Pop-up", font=("Inter", 10),
+                              command=file_error_popup.destroy)
         close_button.config(width="30", height="15")
         close_button.pack(pady=20)
         file_error_popup.grab_set()
@@ -93,9 +90,11 @@ def start_operation():
         folder_error_popup = Toplevel(gui.window)
         folder_error_popup.geometry(f'{popup_width}x{popup_height}+{int(x)}+{int(y)}')
         folder_error_popup.title("Folder Input Error")
-        label = Label(folder_error_popup, text="Please select the folder destination to output to.", font=("Inter", 10))
+        label = Label(folder_error_popup, text="Please select the folder destination to output to.",
+                      font=("Inter", 10))
         label.pack(padx=20, pady=20)
-        close_button = Button(folder_error_popup, text="Close Pop-up", font=("Inter", 10), command=folder_error_popup.destroy)
+        close_button = Button(folder_error_popup, text="Close Pop-up", font=("Inter", 10),
+                              command=folder_error_popup.destroy)
         close_button.config(width="30", height="15")
         close_button.pack(pady=20)
         folder_error_popup.grab_set()
@@ -104,9 +103,12 @@ def start_operation():
         file_and_folder_error_popup = Toplevel(gui.window)
         file_and_folder_error_popup.geometry(f'{popup_width}x{popup_height}+{int(x)}+{int(y)}')
         file_and_folder_error_popup.title("File and Folder Input Error")
-        label = Label(file_and_folder_error_popup, text="Please select the file you wish to input and the folder destination to output to.", font=("Inter", 10))
+        label = Label(file_and_folder_error_popup,
+                      text="Please select the file you wish to input and the folder destination to output to.",
+                      font=("Inter", 10))
         label.pack(padx=20, pady=20)
-        close_button = Button(file_and_folder_error_popup, text="Close Pop-up", font=("Inter", 10), command=file_and_folder_error_popup.destroy)
+        close_button = Button(file_and_folder_error_popup, text="Close Pop-up", font=("Inter", 10),
+                              command=file_and_folder_error_popup.destroy)
         close_button.config(width="30", height="15")
         close_button.pack(pady=20)
         file_and_folder_error_popup.grab_set()
@@ -120,10 +122,16 @@ def start_operation():
         saf_completion_popup = Toplevel(gui.window)
         saf_completion_popup.geometry(f'{popup_width}x{popup_height}+{int(x)}+{int(y)}')
         saf_completion_popup.title("SAF Operation Completed")
-        label = Label(saf_completion_popup, text="SAF files have been created in '" + gui.selected_folder + "' .", font=("Inter", 10))
+        label = Label(saf_completion_popup, text="SAF files have been created in '" + gui.selected_folder + "' .",
+                      font=("Inter", 10))
         label.pack(padx=20, pady=20)
-        close_button = Button(saf_completion_popup, text="Close Pop-up", font=("Inter", 10), command=saf_completion_popup.destroy)
+        close_button = Button(saf_completion_popup, text="Close Pop-up", font=("Inter", 10),
+                              command=saf_completion_popup.destroy)
         close_button.config(width="30", height="15")
         close_button.pack(pady=20)
         saf_completion_popup.grab_set()
         gui.window.wait_window(saf_completion_popup)
+        if gui.open_folder_var.get() == "On":
+            opt.open_output_folder()
+        if gui.close_app_var.get() == "On":
+            opt.close_application()
