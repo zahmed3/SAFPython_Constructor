d1f5edb16cd9a676f6757b42456c6acf81604f3c
zahmed3 <99377958+zahmed3@users.noreply.github.com>
2025-04-21 23:02:58 -0400

    added functionality to remaining two functions, will make program unresponsive while creating zip but will fix after zip archive is made

--- options.py
+++ options.py
@@ -1,19 +1,25 @@
 import gui as gui
 import os
+import os.path
+import saf
+import shutil
 
 #Will alert the user with a pop-up on operation run if they want to continue with operation or end operation early.
 def alert_unnecessary_files():
-    #read files
+    gui.unnecessary_file_count = 0
+    input_folder_path = os.path.dirname(gui.selected_file)
+    for file in os.listdir(input_folder_path):
+        if not(file.endswith(".pdf") or file.endswith(".csv") or file.endswith(".docx") or file.endswith(".mp3")
+               or file.endswith(".mp4")):
+            gui.unnecessary_file_count += 1
 
-    #if none found, pop up saying none found
-
-    #if found, pop up saying some files were found
-
-    pass
-
-#May move to saf if it does not work properly
+#Creates ZIP archives of the operation's output within the SAF folder.
 def create_zip():
-    pass
+    saf_folder_path = os.path.join(gui.selected_folder + "/SimpleArchiveFormat")
+    for folder in os.listdir(saf_folder_path):
+        if folder in os.listdir(saf_folder_path) and os.path.isdir(os.path.join(saf_folder_path, folder)):
+            shutil.make_archive(os.path.join(saf_folder_path, folder), 'zip',
+                                os.path.join(saf_folder_path, folder))
 
 #This function will close the application when the button is pressed.
 def close_application():
