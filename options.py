c32cb81b568f7e1bbb8d93df28e72bb40994c559
zahmed3 <99377958+zahmed3@users.noreply.github.com>
2025-04-23 00:10:09 -0400

    added new function "check_filenames_from_csv" and other minor changes

--- options.py
+++ options.py
@@ -3,6 +3,7 @@ import os
 import os.path
 import saf
 import shutil
+import pandas as pd
 
 #Will alert the user with a pop-up on operation run if they want to continue with operation or end operation early.
 def alert_unnecessary_files():
@@ -10,7 +11,7 @@ def alert_unnecessary_files():
     input_folder_path = os.path.dirname(gui.selected_file)
     for file in os.listdir(input_folder_path):
         if not(file.endswith(".pdf") or file.endswith(".csv") or file.endswith(".docx") or file.endswith(".mp3")
-               or file.endswith(".mp4")):
+               or file.endswith(".mp4") or file.endswith(".zip")):
             gui.unnecessary_file_count += 1
 
 #Creates ZIP archives of the operation's output within the SAF folder.
@@ -28,3 +29,23 @@ def close_application():
 #Opens the folder containing the output when the option is checked.
 def open_output_folder():
     os.startfile(gui.selected_folder)
+
+#Will check if there are any mismatched files between the CSV file and input folder and alert the user if mismatched.
+def check_filenames_from_csv():
+    df = pd.read_csv(gui.selected_file, usecols=["filename"])
+    updated_df = df[df["filename"].notnull()]
+    csv_filename_list = updated_df["filename"].tolist()
+    updated_csv_filename_list = []
+    for line in csv_filename_list:
+        for item in line.split("||"):
+            updated_csv_filename_list.append(item)
+
+    input_folder_filename_list = []
+    input_folder_path = os.path.dirname(gui.selected_file)
+    for file in os.listdir(input_folder_path):
+        if (file.endswith(".pdf") or file.endswith(".docx") or file.endswith(".mp3") or file.endswith(".mp4")
+                or file.endswith(".zip")):
+            input_folder_filename_list.append(file)
+
+    mismatched_filename_list = list(set(updated_csv_filename_list).difference(input_folder_filename_list))
+    return mismatched_filename_list
